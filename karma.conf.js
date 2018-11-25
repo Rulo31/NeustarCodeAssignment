745efb4abf74bd606989968c0a6b71775f58ef31
Raul Gomez <raul.gomez@cheetahdigital.com>
2018-11-25 14:09:39 -0600

    Adding Karma Spec Reporter

--- karma.conf.js
+++ karma.conf.js
@@ -42,12 +42,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec'],
 
 
     // web server port
     port: 9876,
-
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
